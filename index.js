588c19f09e96dc96e6b6d8752f47e7f4ded55f96
bigint <69431456+bigint@users.noreply.github.com>
2023-04-23 19:00:00 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const fileName = "3";
+const fileName = "1";
 const urls = fs
   .readFileSync(`profiles/${fileName}.txt`, "utf-8")
   .split("\n")
@@ -9,12 +9,16 @@ const urls = fs
 const xmlHeader = '<?xml version="1.0" encoding="UTF-8"?>\n';
 const urlsetHeader =
   '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-const urlsetFooter = "</urlset>\n";
+const urlsetFooter = "\n</urlset>\n";
 
 let sitemapXml = xmlHeader + urlsetHeader;
 
-urls.forEach((url) => {
-  sitemapXml += `<url><loc>${url.trim()}</loc></url>\n`;
+urls.map((url, index) => {
+  sitemapXml += `${index === 0 ? "" : "\n"}\t<url>
+    <loc>${url.trim()}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>`;
 });
 
 sitemapXml += urlsetFooter;
